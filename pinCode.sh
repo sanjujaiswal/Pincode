13ddccaccbcab2b8fc442249e63910afb583260c
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-12 18:18:03 +0530

    ADDED: Restrict alphabet and symbol at end

--- pinCode.sh
+++ pinCode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 echo "---Pin code validation program---"
 
-#Constants /It wil not take any character or special symbol at beginning
+#Constants /It wil not take any character or special symbol at end
 PINCODE_PATTERN="^[1-9]{1}[0-9]{5}$"
 
 #Validate Pincode
