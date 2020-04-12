b3d8a5afe86d7f63a1bbe4ed64435d0bb5d9cb51
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-12 14:23:27 +0530

    ADDED: Pincode must be 6 digits

--- pinCode.sh
+++ pinCode.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -x
 echo "---Pin code validation program---"
+
+#Constants declarations/PinCode pattern
+PINCODE_PATTERN="^[1-9]{1}$"
+
+#Validate Pincode
+read -p "Enter PinCode : " pincode
+
+if [[ $pincode =~ $PINCODE_PATTERN ]]
+then
+   echo "Valid PinCode"
+else
+   echo "Invalid PinCode!!"
+fi
